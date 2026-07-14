cb6bbec4798094cee3008a9e8886e3249f35be39
d-khelifaoui <d-khelifaoui@enageo.com>
2026-07-14 10:42:45 +0100

    Update configuration for Vercel deployment

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import tailwindcss from '@tailwindcss/vite';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/Portfolio-/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
